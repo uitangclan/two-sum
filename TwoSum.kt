14f3642f1b4130e7cecbaa4dbf774878ea265abf
uitangclan <uitangclan@gmail.com>
2024-09-30 06:09:46 +0900

    happy coding

--- TwoSum.kt
+++ TwoSum.kt
@@ -6,14 +6,12 @@ fun main (){
 
 fun twoSum(nums: IntArray, target: Int):IntArray {
     var map = HashMap<Int, Int> ()
-    for (i in nums.indices) {
-        map.put(nums[i], i)
-    }
-    for (i in nums.indices) {
-        var j = target - nums[i]
-        if (map.containsKey(j) && map.get(j) != i) {
-            return intArrayOf(i, map.get(j)!!) 
+    for (i in nums.indices ) {
+        var x = target - nums[i]
+        if (map.containsKey(x)) {
+            return intArrayOf(map.get(x)!!, i)
         }
+        map.put(nums[i], i)
     }
     return nums.clone()
 }
